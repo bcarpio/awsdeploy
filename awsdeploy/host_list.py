9fd1b08afc0c03e821284e84fdb2db95ba9009ee
Brian Carpio <bcarpio@thetek.net>
2012-09-28 14:08:44 -0700

    added hosts_list.py

---
+++ awsdeploy/host_list.py
@@ -0,0 +1,12 @@
+from fabric.api import *
+from fabric.operations import local,put
+import paramiko
+import socket
+import subprocess
+import os
+
+@task
+def pqa():
+    social_pqa_ip_list = local("ldapsearch -x -w secret -D 'cn=admin,dc=social,dc=local' -b 'ou=hosts,dc=social,dc=local' -h 10.252.2.165  -LLL  cn=usw2* ipHostNumber | grep ipHostNumber | awk '{print $2}'", capture=True).splitlines()
+    env.user = 'ubuntu'
+    env.hosts = social_pqa_ip_list
