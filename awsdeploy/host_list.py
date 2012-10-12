f910a9583165d50b7a4ae6c714873d8a37830cdb
Brian Carpio <bcarpio@thetek.net>
2012-10-12 14:39:03 -0700

    updated host list for prod

--- awsdeploy/host_list.py
+++ awsdeploy/host_list.py
@@ -15,3 +15,8 @@ def pqa_nopuppet_master():
     social_pqa_ip_list = local("ldapsearch -x -w secret -D 'cn=admin,dc=social,dc=local' -b 'ou=hosts,dc=social,dc=local' -h 10.252.2.165  -LLL  cn=usw2* ipHostNumber | grep ipHostNumber | grep -v 10.252.2.27 | awk '{print $2}'", capture=True).splitlines()
     env.user = 'ubuntu'
     env.hosts = social_pqa_ip_list
+@task
+def prod():
+    social_prod_ip_list = local("ldapsearch -x -w secret -D 'cn=admin,dc=social,dc=local' -b 'ou=hosts,dc=social,dc=local' -h 10.201.2.176  -LLL  cn=use1* ipHostNumber | grep ipHostNumber | awk '{print $2}'", capture=True).splitlines()
+    env.user = 'ubuntu'
+    env.hosts = social_prod_ip_list
