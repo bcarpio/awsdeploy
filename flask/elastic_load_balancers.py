33baa67348667eb9ad629489df5a6b0de79e92bb
Brian Carpio <brianc@ecollege.com>
2012-12-12 23:52:14 -0700

    updated links for AWS and elastic load balacner

--- flask/elastic_load_balancers.py
+++ flask/elastic_load_balancers.py
@@ -7,9 +7,9 @@ import os, sys
 sys.path.append('../')
 from awsdeploy import *
 import boto.ec2.elb
+import boto.vpc
 
 def elastic_load_balacner_list(region):
-
     creds = config.get_ec2_conf()
     conn = boto.ec2.elb.connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
     elbs = conn.get_all_load_balancers()
@@ -26,7 +26,22 @@ def elastic_load_balacner_list(region):
         dns_name = elb.dns_name
         health_check = elb.health_check
         listeners = elb.listeners
-        elb_info = { 'name' : elb.name, 'attached_instance_health' : instance_health_list, 'dns_name' : dns_name , 'health_check' : health_check, 'listeners' : listeners}
+        subnets = elb.subnets
+        regions = boto.ec2.regions(aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
+        for region_info in regions:
+            if region_info.name == region:
+                vpc_region = region_info
+                break
+        vpc_conn = boto.vpc.VPCConnection(aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'],region=vpc_region)
+        subnets_info = vpc_conn.get_all_subnets()
+        subnet_ips = []
+        for subnet in subnets:
+            for subnet_info in subnets_info:
+                if subnet == subnet_info.id:
+                    subnet_ip = subnet_info.cidr_block
+                    subnet_ips.append(subnet_ip)
+            
+        elb_info = { 'name' : elb.name, 'attached_instance_health' : instance_health_list, 'dns_name' : dns_name , 'health_check' : health_check, 'listeners' : listeners, 'subnet_ips' : subnet_ips}
         elbs_info.append(elb_info)
     inst_conn = connect_to_region(region, aws_access_key_id=creds['AWS_ACCESS_KEY_ID'], aws_secret_access_key=creds['AWS_SECRET_ACCESS_KEY'])
     instance_name = inst_conn.get_all_instances()
@@ -40,3 +55,5 @@ def elastic_load_balacner_list(region):
                         name = None
                     instance_id_list['instance_name'] = name
     return elbs_info
+
+    
